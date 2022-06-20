2f566996f6bda3b1ea7bdf6f21c6cc134dc0393e
aaronlyy <aaronlevican@gmail.com>
2022-06-20 15:59:13 +0200

    arrays update

--- basics/arrays.js
+++ basics/arrays.js
@@ -2,3 +2,28 @@ let cars = ["bmw", "mercedes", "ferrari"];
 
 // access array on index
 console.log(cars[0]);
+
+// re-assign values
+cars[0] = "skoda";
+
+// get the length of an array
+console.log(cars.length);
+
+// add an element
+cars.push("fiat");
+
+// add an element at index 0
+cars.unshift("mini");
+
+// remove the last element
+cars.pop();
+
+// remove first element
+cars.shift();
+
+// find index of an element
+console.log(cars.indexOf("ferrari"));
+
+// check if variable is an array
+console.log(Array.isArray(cars));
+
