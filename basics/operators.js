ace1df89c77d1b2eba3638e719bce6de0aff7eb7
aaronlyy <aaronlevican@gmail.com>
2022-06-20 16:55:33 +0200

    added operators

---
+++ basics/operators.js
@@ -0,0 +1,15 @@
+console.log(15 + 3);
+console.log(15 - 3);
+console.log(15 * 3);
+console.log(15 / 3);
+console.log(15 ** 3);
+console.log(15 % 3);
+console.log(15 > 3);
+console.log(15 < 3);
+console.log(15 >= 3);
+console.log(15 <= 3);
+console.log(15 != 3);
+console.log(15 == 3);
+console.log(15 === 3);
+
+console.log(("b" + "a" + + "b" + "a").toLowerCase());
