8eeec2c99ddf9ef3a0563e6aca89e7e5b5dbbb15
aaronlyy <aaronlevican@gmail.com>
2022-06-20 18:15:13 +0200

    added if & switch

---
+++ basics/if.js
@@ -0,0 +1,17 @@
+const age = 18;
+
+if (age >= 18) {
+    console.log("let's drink!");
+}
+else if (age >= 16) {
+    console.log("let's get some beers!");
+}
+else {
+    console.log("he need some milk!");
+}
+
+
+const x = true ? age >= 18 : false;
+if (x) {
+    console.log("yeet!");
+}
