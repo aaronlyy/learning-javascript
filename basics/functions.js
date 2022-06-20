991f2dd760fdbc50a5742c70e0eb93f88f40c5be
aaronlyy <aaronlevican@gmail.com>
2022-06-20 18:37:59 +0200

    added anon functions

--- basics/functions.js
+++ basics/functions.js
@@ -14,3 +14,16 @@ console.log(x);
 console.log(say("aaron"));
 
 // anonymous functions
+
+let before = function () {
+    console.log("before ES6");
+}
+
+let after = (n=1) => {
+    for (let i = 0; i < n; i++) {
+        console.log("after ES6");
+    }
+}
+
+before();
+after(3);
